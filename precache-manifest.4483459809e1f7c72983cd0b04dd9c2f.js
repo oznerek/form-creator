3eef40c810784490b939805fab7427ced7e207fc
oznerek <oznerek@gmail.com>
2019-08-10 08:37:36 +0200

    Updates

--- precache-manifest.4483459809e1f7c72983cd0b04dd9c2f.js
+++ precache-manifest.4483459809e1f7c72983cd0b04dd9c2f.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/form-creator/static/js/runtime~main.d3582e25.js"
   },
   {
-    "revision": "d880f245312481159b80",
-    "url": "/form-creator/static/js/main.d880f245.chunk.js"
+    "revision": "130d7a8ada3013f765e1",
+    "url": "/form-creator/static/js/main.130d7a8a.chunk.js"
   },
   {
     "revision": "4e2c2a00bd81b45056c6",
     "url": "/form-creator/static/js/2.4e2c2a00.chunk.js"
   },
   {
-    "revision": "9ffaddace0abd0b2930e78f158854311",
+    "revision": "b53809e628cdcde81ea0ccc1e28e7ff6",
     "url": "/form-creator/index.html"
   }
 ];
